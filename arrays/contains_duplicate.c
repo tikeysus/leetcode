f1edc47fe348bf6ea244b82e4a24a7788f27965e
Tigran Tumanyan <tigrantumanyann@gmail.com>
2024-06-26 09:05:30 -0400

    Only works from -9 to 9, will improve on this.

--- arrays/contains_duplicate.c
+++ arrays/contains_duplicate.c
@@ -8,14 +8,21 @@ int main(){
 }
 
 int duplicate(int* nums, int numsSize){
-	int occurences[10] = {0}; //can be made to be 256 if ASCII characters are to be considered
+	int occurences[19] = {0}; //can be made to be 256 if ASCII characters are to be considered
 
 	for (int i = 0; i < numsSize; i++){
-		int current = nums[i]; 
-		occurences[current] += 1; 
+		if (nums[i] >= 0){
+			int current = nums[i] + 10; 
+			occurences[current] += 1; 
+		}
+
+		else{
+			int current = nums[i] + 10; 
+			occurences[current] += 1; 
+		}
 	}
 
-	for (int i = 0; i < 10; i++){
+	for (int i = 0; i < 19; i++){
 		if (occurences[i] >= 2){
 			return 1; 
 		}
