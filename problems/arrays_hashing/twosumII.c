298dc6a9e7212602f3e7d4b3cfe5cc323a09fbd0
Tigran Tumanyan <tigrantumanyann@gmail.com>
2026-04-27 13:17:45 -0400

    cleaing up

---
+++ problems/arrays_hashing/twosumII.c
@@ -0,0 +1,43 @@
+/*
+Problem: twosumII
+Link:
+Difficulty:
+Topic(s):
+Pattern:
+
+Description:
+
+Constraints:
+
+Initial idea:
+
+Final approach:
+
+Complexity:
+Time:
+Space:
+
+Mistakes / difficulties:
+
+Key lesson:
+
+Review:
+- Solved on:
+- Revisit:
+*/
+
+#include <stdio.h>
+
+int* twoSum(int* numbers, int numbersSize, int target, int* returnSize) {
+    // your code
+}
+
+int main(void) {
+    int nums[] = {2, 7, 11, 15};
+    int returnSize = 0;
+
+    int* result = twoSum(nums, 4, 9, &returnSize);
+
+    printf("%d %d\n", result[0], result[1]);
+    return 0;
+}
