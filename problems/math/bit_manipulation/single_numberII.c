2f8c83906ad981722f1d163eddc22b0410106b6a
Tigran Tumanyan <tigrantumanyann@gmail.com>
2026-05-01 17:22:35 -0400

    for later.

---
+++ problems/math/bit_manipulation/single_numberII.c
@@ -0,0 +1,41 @@
+/*
+Problem: single numberII
+Link:
+Difficulty: Medium 
+Topic(s):
+Pattern:
+
+Description:
+
+Constraints:
+
+Initial idea:
+This smells like an ASCII array. But these aren't chars, they are numbers, which means I have to change my approach. 
+Hashing in python would make this a lot easier. 
+
+Final approach:
+
+Complexity:
+Time:
+Space:
+
+Mistakes / difficulties:
+
+Key lesson:
+
+Review:
+- Solved on:
+- Revisit:
+*/
+
+#include <stdio.h>
+
+int singleNumber(int* nums, int numsSize) {
+}
+
+int main(){
+	int nums[] = {2,3,2,2}; 
+	int numsSize = 4; 
+	int res = singleNumber(nums, numsSize); 
+	printf("%d\n", res); 
+}
