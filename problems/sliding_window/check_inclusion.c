ce0a5ba9f724c950b97fdefa322de3707391be56
Tigran Tumanyan <tigrantumanyann@gmail.com>
2026-05-26 17:13:54 -0400

    very sloppy, thought I could squeeze it in today.

--- problems/sliding_window/check_inclusion.c
+++ problems/sliding_window/check_inclusion.c
@@ -32,13 +32,54 @@ Review:
 #include <stdio.h>
 #include <stdbool.h>
 
+// int* ascii_arr_builder(char* s){
+// 	int ascii_arr[28] = {0}; //we know the size of the array here, so iteration through it will be safer later on. 
+// 	for (int i = 0; s[i] != '\0'; i++){
+// 		ascii_arr[s[i] - 'a']++; 
+// 	}
+// 	return ascii_arr; 
+// }
+
+bool array_compare(int* s1_ascii_arr, int* s2_ascii_arr){
+	for (int i = 0; i < 28; i++){
+		if (s1_ascii_arr[i] != s2_ascii_arr[i]){
+			return false; 
+		}
+	}
+	return true; 
+}
+
 bool checkInclusion(char* s1, char* s2) {
-    
+	int s1_ascii_arr[28] = {0};
+	for (int i = 0; s1[i] != '\0'; i++){
+		s1_ascii_arr[s1[i] - 'a']++; 
+	}
+	int s2_ascii_arr[28] = {0}; 
+	
+	for (int right = 0; s2[right] != '\0'; right++){
+		if (s1_ascii_arr[s2[right] - 'a'] == 0){
+			continue; 
+		}
+		else{
+			while (s1_ascii_arr[s2[right] - 'a'] != 0){
+				s2_ascii_arr[s2[right] - 'a']++; 
+				right++; 
+			}
+		}
+
+		if (array_compare(s1_ascii_arr, s2_ascii_arr) == 1){
+			return true; 
+		}
+		for (int i = 0; i < 28; i++){
+			s2_ascii_arr[i] = 0; 
+		}
+	}
+	return false; 
 }
 
 int main(){
-	char s1[3] = "ab";
-	char s2[9] = "eidbaooo";
+	char s1[4] = "adc";
+	char s2[5] = "dcda";
 	bool res = checkInclusion(s1, s2); 
 	printf("%d\n", res); 
 	return 0; 
