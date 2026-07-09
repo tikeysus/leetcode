d8ee1ea8901237346da5cf5b7979f74f4fa7f227
Tigran Tumanyan <tigrantumanyann@gmail.com>
2026-07-09 10:54:36 -0400

    Really interesting problem, will have to come back to it tomorrow.

--- problems/math/bit_manipulation/single_number_ii.c
+++ problems/math/bit_manipulation/single_number_ii.c
@@ -31,6 +31,20 @@ Review:
 #include <stdio.h>
 
 int singleNumber(int* nums, int numsSize) {
+	int result = 0; 
+	for (int i = 0; i < 32; i++){
+		int count = 0; 
+		for (int j = 0; j < numsSize; j++){
+			if ((nums[j] >> i) & 1){
+				count++; 
+			}
+		}
+
+		if ((count % 3) == 1){
+			result |= (1LL << i); 
+		}
+	}
+	return result; 
 }
 
 int main(){
