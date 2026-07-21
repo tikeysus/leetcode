636ee2da3c3942c9cfe46bf8aa50cab30af88858
Tigran Tumanyan <tigrantumanyann@gmail.com>
2026-07-20 21:46:11 -0400

    I will have to scrap this whole thing because it turns out that the integers can be of any size. So will need to do addition with carry logic maybe?

--- problems/arrays/linked_lists/add_two_numbers.c
+++ problems/arrays/linked_lists/add_two_numbers.c
@@ -39,39 +39,49 @@ struct ListNode {
 	struct ListNode *next;
 };
 
-long long linked_to_int(struct ListNode* l){
-	long long acc = 0; 
-	while (l != NULL){
-		acc += l->val;
-		if (l->next != NULL) { acc *= 10; }
-		l = l->next; 
-	}
-	return acc; 
+long long linked_to_int(struct ListNode* l) {
+    unsigned long long acc = 0;
+
+    acc += l->val;
+    l = l->next;
+    unsigned long long i = 1;
+    while (l != NULL) {
+        acc += (l->val) * (10LL * i);
+        l = l->next;
+        i *= 10;
+    }
+    return acc;
 }
 
 struct ListNode* int_to_linked(long long res) {
-	long long res_copy = res; 
+	unsigned long long res_copy = res; 
 
-	struct ListNode *node_digit = (struct ListNode*)(malloc(sizeof(struct ListNode*))); 
-	node_digit->next = NULL; 
+	struct ListNode *sum = (struct ListNode*)(malloc(sizeof(struct ListNode))); 
+	sum->next = NULL; 
+	struct ListNode *answer = sum; 
 
-	int i = 1; 
+	unsigned long long i = 1; 
 	while (res_copy != 0){
 		int digit = (res % (10LL * i)) / i; 
-		node_digit->val = digit; 
 
-		struct ListNode *new_node = (struct ListNode*)(malloc(sizeof(struct ListNode*))); 
-		new_node -> next = node_digit; 
+		sum->val = digit;
+		struct ListNode *new_node = (struct ListNode*)(malloc(sizeof(struct ListNode))); 
+		new_node->next = NULL; 
+		sum->next = new_node; 
+        if (res_copy/10 == 0) { sum -> next = NULL; }
+        else { sum = sum->next; }
+		
 
-		i *= 10; res_copy /= 10; 
+		i *= 10LL; res_copy /= 10LL; 
 	}
+	return answer; 
 }
 
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
-	if (l1->val == 0) { return l2; }
-	if (l2->val == 0) { return l1; }
-	
-	long long l1_int = linked_to_int(l1);
-	long long l2_int = linked_to_int(l2);
-	long long res = l2_int + l1_int; 
+    if (l1->val == 0 && l1->next == NULL){ return l2; }
+    if (l2->val == 0 && l2->next == NULL){ return l1; }
+    unsigned long long l1_int = linked_to_int(l1);
+    unsigned long long l2_int = linked_to_int(l2);
+    unsigned long long res = l2_int + l1_int;
+    return int_to_linked(res);
 }
