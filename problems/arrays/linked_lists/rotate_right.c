d2256c0c64505a5cefbcc625ffff69890736d8a0
Tigran Tumanyan <tigrantumanyann@gmail.com>
2026-07-22 17:06:52 -0400

    Will first try rotate array, see if that clarifies things.

---
+++ problems/arrays/linked_lists/rotate_right.c
@@ -0,0 +1,42 @@
+/*
+Problem: rotate right
+Link:
+Difficulty:
+Topic(s):
+Pattern:
+
+Description:
+Given the head of a linked list, rotate the list to the right by k places.
+
+Constraints:
+The number of nodes in the list is in the range [0, 500].
+-100 <= Node.val <= 100
+0 <= k <= 2 * 109
+
+Initial idea:
+
+Final approach:
+
+Complexity:
+Time:
+Space:
+
+Mistakes / difficulties:
+
+Key lesson:
+
+Review:
+- Solved on:
+- Revisit:
+*/
+
+#include <stdlib.h>
+
+struct ListNode{
+	int val;
+	struct ListNode *next;
+};
+
+struct ListNode* rotateRight(struct ListNode* head, int k) {
+	
+}
