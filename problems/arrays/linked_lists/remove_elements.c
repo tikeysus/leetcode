8ec539903e638ea7994eeacc1fa78a56f860269a
Tigran Tumanyan <tigrantumanyann@gmail.com>
2026-07-23 12:44:42 -0400

    Too much linked list shinenigans, I will return to this later.

---
+++ problems/arrays/linked_lists/remove_elements.c
@@ -0,0 +1,38 @@
+/*
+Problem: remove elements
+Link:
+Difficulty:
+Topic(s):
+Pattern:
+
+Description:
+Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+Constraints:
+
+Initial idea:
+
+Final approach:
+
+Complexity:
+Time:
+Space:
+
+Mistakes / difficulties:
+
+Key lesson:
+
+Review:
+- Solved on:
+- Revisit:
+*/
+
+#include <stdlib.h>
+
+struct ListNode {
+	int val;
+	struct ListNode *next;
+};
+
+struct ListNode* removeElements(struct ListNode* head, int val) {
+}
